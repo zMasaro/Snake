f4930a0a44ed8f33e21f3ef56060939561bca0ad
Emmanuel <emmannuell.sanchhez@gmail.com>
2024-11-03 10:57:51 -0600

    Avance de la clase GameStatus con todos sus metodos de collision y estado de la partida

--- Model/GameStates.java
+++ Model/GameStates.java
@@ -9,5 +9,59 @@ package Model;
  * @author emman
  */
 public class GameStates {
+
+    private boolean running = true;
+    private Snake snake = new Snake();
+    private Food food = new Food();
+    private Obstacle obstacle;
+
+    public GameStates() {
+        food.spawn(GameConfig.PARALLEL_FRAMES, GameConfig.PARALLEL_FRAMES);
+    }
+
+    public void checkCollision() {
+        if (checkWallCollision() || checkSelfCollision() || checkObstacleCollision()) {
+            running = false;
+        }
+    }
+
+    public boolean checkWallCollision() {
+        return snake.getSnakeX()[0] < 0 || snake.getSnakeY()[0] < 0 || snake.getSnakeX()[0] >= GameConfig.SCREEN || snake.getSnakeY()[0] >= GameConfig.SCREEN;
+    }
+
+    public boolean checkSelfCollision() {
+        for (int i = snake.getSnakeBody(); i > 0; i--) {
+            if (snake.getSnakeX()[0] == snake.getSnakeX()[i] && snake.getSnakeY()[0] == snake.getSnakeY()[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void checkFoodCollision() {
+        if (snake.getSnakeX()[0] == food.getX() && snake.getSnakeY()[0] == food.getY()) {
+            snake.grow();
+            food.spawn(GameConfig.PARALLEL_FRAMES, GameConfig.PARALLEL_FRAMES);
+        }
+    }
     
+    public boolean checkObstacleCollision() {
+        return snake.getSnakeX()[0] == obstacle.getX() && snake.getSnakeY()[0] == obstacle.getY();
+    }
+
+    public Snake getSnake() {
+        return snake;
+    }
+
+    public Food getFood() {
+        return food;
+    }
+
+    public boolean isRunning() {
+        return running;
+    }
+
+    public void setRunning(boolean running) {
+        this.running = running;
+    }
 }
